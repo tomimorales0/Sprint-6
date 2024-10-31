609c00188962565267fdaacdd685b2697c3ec752
Aromzio <aron231104@gmail.com>
2024-10-31 13:44:13 -0300

    Update2 consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -101,7 +101,7 @@ JOIN sucursal AS s ON c.branch_id = s.branch_id
 GROUP BY s.branch_name
 ORDER BY customer_count DESC;
 
-
+//
 
 
 SELECT s.branch_name, COUNT(e.employee_id) / COUNT(c.customer_id) AS employees_per_client
@@ -110,6 +110,7 @@ JOIN sucursal AS s ON e.branch_id = s.branch_id
 JOIN cliente AS c ON c.branch_id = s.branch_id
 GROUP BY s.branch_name;
 
+//
 
 SELECT sucursal, COUNT(card_id) AS num_credit_cards
 FROM tarjetas
@@ -118,7 +119,7 @@ ORDER BY num_credit_cards DESC;
 
 
 
-//Cantidad de tarjetas de crédito por tipo por sucursal??
+//
 
 
 
@@ -128,33 +129,37 @@ JOIN sucursal AS s ON p.loan_id = s.branch_id
 GROUP BY s.branch_name;
 
 
+//
+    
+UPDATE cuenta
+SET balance = balance - 100
+WHERE account_id BETWEEN 10 AND 14;
+
+//
+
+BEGIN TRANSACTION;
+UPDATE cuenta
+
+SET balance = balance - 1000
+WHERE account_id = 200;
+UPDATE cuenta
+SET balance = balance + 1000 
+WHERE account_id = 400;
 
 
-CREATE TRIGGER IF NOT EXISTS audit_update_cuenta
-AFTER UPDATE OF balance, iban ON cuenta
-FOR EACH ROW
+INSERT INTO movimientos (movements_id, account_number, amount, operation_type, transaction_time)
+VALUES 
+(1, 200, -1000, 'transferencia', CURRENT_TIMESTAMP), 
+(2, 400, +1000, 'deposito', CURRENT_TIMESTAMP);
+
+
+
+IF @@ERROR = True
 BEGIN
-    INSERT INTO auditoria_cuenta (
-        old_id, 
-        new_id, 
-        old_balance, 
-        new_balance, 
-        old_iban, 
-        new_iban, 
-        old_type, 
-        new_type, 
-        user_action, 
-        created_at
-    ) VALUES (
-        OLD.account_id, 
-        NEW.account_id, 
-        OLD.balance, 
-        NEW.balance, 
-        OLD.iban, 
-        NEW.iban, 
-        OLD.type, 
-        NEW.type, 
-        'UPDATE', 
-        CURRENT_TIMESTAMP
-    );
-END;
+
+    ROLLBACK TRANSACTION;
+END
+ELSE
+BEGIN
+    COMMIT TRANSACTION;
+END
